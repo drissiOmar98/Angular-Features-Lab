bd07151d801f40f860c4c0037b8099acee19bb54
DrissiOmar <omar.drissi@esprit.tn>
2025-12-18 17:13:48 +0100

    feat(router): setup application routes with lazy-loaded features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {Home} from './home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'resource-api',
+    loadChildren: () => import('./examples/resource-api/resource-api.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
