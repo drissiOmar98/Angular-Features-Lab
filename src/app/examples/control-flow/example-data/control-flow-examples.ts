da5f58c1a79ec54753372e66819be8ed025ae10b
DrissiOmar <omar.drissi@esprit.tn>
2026-02-12 22:10:18 +0100

    feat(ng-template): add NgTemplate Button example to demonstrate reusable templates and reduce repetitive code

---
+++ src/app/examples/control-flow/example-data/control-flow-examples.ts
@@ -0,0 +1,8 @@
+import {ExampleModel} from '../../../shared/models/example.model';
+
+export const CONTROL_FLOW_EXAMPLES: ExampleModel[] = [
+
+
+  {title: 'NgTemplate Button Example', content: 'Avoid repetitive code by reusing ng-templates with ngTemplateOutlet for buttons and similar elements.', routerLink: '/control-flow/example2'}
+
+];
